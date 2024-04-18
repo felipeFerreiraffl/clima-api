90a8773c7e4392f5114e6d44340b59cb25fa949d
= <=>
2024-04-17 21:53:37 -0300

    feat: created a weather API

---
+++ src/scripts/script-get.js
@@ -0,0 +1,36 @@
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid={4d04b0dc4b016dbde26edb8ae6abd73a}&units=metric';
+
+// const obterTemp = () =>{
+
+//   fetch(url)
+//         .then(resposta =>{
+//           // console.log(resposta)
+//           return resposta.json()
+//         })
+//           .then(dados =>{
+//             console.log(dados)
+//           })
+//           .catch(erro => {
+//             console.log(erro)
+//           })
+//  }
+
+const obterTemp = () =>{
+
+    const nome = document.querySelector('#name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${nome}&lang=pt_br&units=metric&appid=777fd6c175f16899b669ab9b22be7638`;
+
+    fetch(url)
+      .then(resposta =>{
+        // console.log(resposta)
+        return resposta.json()
+      })
+        .then(dados =>{
+          console.log(dados);
+          document.querySelector('#temp').value = dados.main.temp + ' °C';
+
+        })
+        .catch(erro => {
+          console.log(erro)
+        })
+  }
