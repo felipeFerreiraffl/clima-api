708d854e9afc96b83f81bb37b0a9db5f8a73ec51
Felipe Ferreira <felipado2x0@gmail.com>
2024-04-27 20:07:39 -0300

    docs: documented the code to better undestanding

--- src/scripts/script-get.js
+++ src/scripts/script-get.js
@@ -1,23 +1,7 @@
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid={4d04b0dc4b016dbde26edb8ae6abd73a}&units=metric';
-
-// const obterTemp = () =>{
-
-//   fetch(url)
-//         .then(resposta =>{
-//           // console.log(resposta)
-//           return resposta.json()
-//         })
-//           .then(dados =>{
-//             console.log(dados)
-//           })
-//           .catch(erro => {
-//             console.log(erro)
-//           })
-//  }
-
 const obterTemp = () =>{
-
+    // Declaração do nome do input
     const nome = document.querySelector('#name').value;
+    // Seleção de uma cidade ao digitar no input
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${nome}&lang=pt_br&units=metric&appid=777fd6c175f16899b669ab9b22be7638`;
 
     fetch(url)
@@ -28,10 +12,12 @@ const obterTemp = () =>{
         .then(dados =>{
           console.log(dados);
 
+          // Ao colocar o nome, aparecerá a temperatura atual no output
           document.querySelector('#temp').value = dados.main.temp + ' °C';
         })
         .catch(erro => {
           console.log(erro);
-          alert('Coloca a cidade, burro!');
+          // Caso não seja colocado o nome no output
+          alert('Coloca uma cidade, burro!');
         })
   }
